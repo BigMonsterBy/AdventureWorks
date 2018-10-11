51f341b37563d18df7a60103d4f661741275182b
Youri Roudy <youri_roudy@epam.com>
2018-10-11 20:12:46 +0300

    added sources

---
+++ AdventureWorks.Web/Models/AdventureWorks2017Context.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace AdventureWorks.Web.Models
+{
+    public partial class AdventureWorks2017Context : DbContext
+    {
+        public AdventureWorks2017Context()
+        {
+        }
+
+        public AdventureWorks2017Context(DbContextOptions<AdventureWorks2017Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Product { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=.;Database=AdventureWorks2017;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.ToTable("Product", "Production");
+
+                entity.HasIndex(e => e.Name)
+                    .HasName("AK_Product_Name")
+                    .IsUnique();
+
+                entity.HasIndex(e => e.ProductNumber)
+                    .HasName("AK_Product_ProductNumber")
+                    .IsUnique();
+
+                entity.HasIndex(e => e.Rowguid)
+                    .HasName("AK_Product_rowguid")
+                    .IsUnique();
+
+                entity.Property(e => e.ProductId).HasColumnName("ProductID");
+
+                entity.Property(e => e.Class).HasMaxLength(2);
+
+                entity.Property(e => e.Color).HasMaxLength(15);
+
+                entity.Property(e => e.DiscontinuedDate).HasColumnType("datetime");
+
+                entity.Property(e => e.FinishedGoodsFlag)
+                    .IsRequired()
+                    .HasColumnType("Flag")
+                    .HasDefaultValueSql("((1))");
+
+                entity.Property(e => e.ListPrice).HasColumnType("money");
+
+                entity.Property(e => e.MakeFlag)
+                    .IsRequired()
+                    .HasColumnType("Flag")
+                    .HasDefaultValueSql("((1))");
+
+                entity.Property(e => e.ModifiedDate)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasColumnType("Name")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.ProductLine).HasMaxLength(2);
+
+                entity.Property(e => e.ProductModelId).HasColumnName("ProductModelID");
+
+                entity.Property(e => e.ProductNumber)
+                    .IsRequired()
+                    .HasMaxLength(25);
+
+                entity.Property(e => e.ProductSubcategoryId).HasColumnName("ProductSubcategoryID");
+
+                entity.Property(e => e.Rowguid)
+                    .HasColumnName("rowguid")
+                    .HasDefaultValueSql("(newid())");
+
+                entity.Property(e => e.SellEndDate).HasColumnType("datetime");
+
+                entity.Property(e => e.SellStartDate).HasColumnType("datetime");
+
+                entity.Property(e => e.Size).HasMaxLength(5);
+
+                entity.Property(e => e.SizeUnitMeasureCode).HasMaxLength(3);
+
+                entity.Property(e => e.StandardCost).HasColumnType("money");
+
+                entity.Property(e => e.Style).HasMaxLength(2);
+
+                entity.Property(e => e.Weight).HasColumnType("decimal(8, 2)");
+
+                entity.Property(e => e.WeightUnitMeasureCode).HasMaxLength(3);
+            });
+        }
+    }
+}
