b8e1566720aed3ea920a4ce1927ee3c6b4f17f46
Youri Roudy <youri_roudy@epam.com>
2018-10-31 18:37:18 +0300

    updated to new subscription

--- AdventureWorks.Web/Controllers/DocumentController.cs
+++ AdventureWorks.Web/Controllers/DocumentController.cs
@@ -45,16 +45,16 @@ namespace AdventureWorks.Web.Controllers
                         var uri = await _azureService.AddBlobAsync(stream, formFile.FileName);
                         _logger.LogInformation($"{formFile.FileName} saved: {uri}");
 
-                        var fileMetadata = new FileMetadata
-                        {
-                            Filename = formFile.FileName,
-                            Size = formFile.Length,
-                            UploadedDeate = DateTime.UtcNow,
-                            StoringUri = uri
-                        };
+                        //var fileMetadata = new FileMetadata
+                        //{
+                        //    Filename = formFile.FileName,
+                        //    Size = formFile.Length,
+                        //    UploadedDeate = DateTime.UtcNow,
+                        //    StoringUri = uri
+                        //};
 
-                        await _azureService.AddToQueueAsync(fileMetadata);
-                        _logger.LogInformation($"{formFile.FileName} added to queue");
+                        //await _azureService.AddToQueueAsync(fileMetadata);
+                        //_logger.LogInformation($"{formFile.FileName} added to queue");
                     }
                 }
             }
